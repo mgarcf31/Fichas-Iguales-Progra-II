3d3404e3a4ec9d35abc6ee1ad5f31d1c3e016a31
mgarcf31 <90622939+mgarcf31@users.noreply.github.com>
2023-12-14 15:46:09 +0100

    Update Ficha.java

--- Ficha.java
+++ Ficha.java
@@ -3,17 +3,23 @@ public class Ficha {
     char color;
     char letra;
     Boolean grupo;
+    int fila;
+    int columna;
 
-    public Ficha (char color, char letra, boolean grupo){
+    public Ficha (char color, char letra, boolean grupo, int fila, int columna){
         this.color = color;
         this.letra = letra;
         this.grupo = grupo;
+        this.fila = fila;
+        this.columna = columna;
     }
     
     public Ficha (){
         this.color = ' ';
         this.letra = ' ';
         this.grupo = false;
+        this.fila = 0;
+        this.columna = 0;
     }
 
     public char getColor(){
@@ -28,11 +34,32 @@ public class Ficha {
         return letra;
     }
 
-    public void setColor(char letra){
-        this.color = letra;
+    public int getFila(){
+        return fila;
+    }
+
+    public int getColumna(){
+        return columna;
+    }
+
+    public void setColor(char color){
+        this.color = color;
+    }
+
+    public void setLetra (char letra){
+        this.letra = letra;
     }
 
     public void setGrupo(boolean grupo){
         this.grupo = grupo;
     }
+
+    public void setFila(int fila){
+        this.fila = fila;
+    }
+
+    public void serColumna(int columna){
+        this.columna = columna;
+    }
+
 }
