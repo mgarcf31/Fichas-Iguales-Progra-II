3eb7ab7805213829a64cada1c0b458a35d3196df
mgarcf31 <90622939+mgarcf31@users.noreply.github.com>
2023-12-14 15:47:53 +0100

    Create Movimientos.java

---
+++ Movimientos.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Movimientos {
+    private List<Movimiento> listaMovimientos;
+
+    public Movimientos() {
+        this.listaMovimientos = new ArrayList<>();
+    }
+
+
+    public void agregarMovimiento(int fila, int columna, int numFichas, char color, int puntuacion) {
+        Movimiento nuevoMovimiento = new Movimiento(fila, columna, numFichas, color, puntuacion);
+        listaMovimientos.add(nuevoMovimiento);
+    }
+
+    public void eliminarMovimiento(int fila, int columna, int numFichas, char color, int puntuacion){
+        List<Movimiento> auxiliar = new ArrayList<>();
+        for(Movimiento movimiento : listaMovimientos){
+            if(!(movimiento.fila == fila && movimiento.columna == columna && movimiento.numFichas == numFichas && movimiento.color == color && movimiento.puntuacion == puntuacion)){
+                auxiliar.add(movimiento);
+            }
+        }
+        listaMovimientos = auxiliar;
+    }
+
+    public void ordenarLista(){
+        int n = listaMovimientos.size();
+        for(int i = 0; i < n-1; i++){
+            for(int j = 0; j < n-i-1; j++){
+                // estoy en un movimiento y en el que está a su lado. 
+                if (listaMovimientos.get(j).getPuntuacion() < listaMovimientos.get(j+i).getPuntuacion()){
+                    // intercambio los elementos si están en orden contrario
+                    Movimiento  auxiliar = listaMovimientos.get(j);
+                    listaMovimientos.set(j, listaMovimientos.get(j+1));
+                    listaMovimientos.set(j+1, auxiliar);
+                }
+            }
+        }
+    }    
+    
+}
