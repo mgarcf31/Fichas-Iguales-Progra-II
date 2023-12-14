9defc8f6890aab87a5f0f6d94d746eabc92eca74
mgarcf31 <90622939+mgarcf31@users.noreply.github.com>
2023-12-14 18:07:25 +0100

    Update Tablero.java

--- Tablero.java
+++ Tablero.java
@@ -4,9 +4,20 @@ import java.util.List;
 public class Tablero {
     
     private ArrayList<ArrayList<Ficha>> tablero ;
+    private Ficha ficha;
     private Movimientos movimientos;
     private int filas;
     private int columnas;
+
+
+    public int getFilas() {
+        return filas;
+    }
+
+    public int getColumnas() {
+        return columnas;
+    }
+
     public Tablero(int filas, int columnas){
         this.filas = filas;
         this.columnas = columnas;
@@ -47,38 +58,55 @@ public class Tablero {
     
     }
 
-/*    public ArrayList<ArrayList<int[]>> getMovsPosibles() {// podriamos considerarlo movimiento o grupo
+    public Ficha getFicha(int fila, int columna) {
+        if (fila >= 0 && fila < tablero.size() && columna >= 0 && columna < tablero.get(fila).size()) {
+            return tablero.get(fila).get(columna);
+        } else {
+            // Devuelve una ficha especial o maneja el caso fuera de los límites según tus necesidades
+            return new Ficha(' ', ' ', false, 0, 0);
+        }
+    }
+
+    public ArrayList<ArrayList<int[]>> getMovsPosibles() {
         ArrayList<ArrayList<int[]>> movsPosib = new ArrayList<>();
-        boolean[][] visitado = new boolean[fil][col];
-        for (int i = 0; i < fil; i++) {
-            for (int j = 0; j < col; j++) {
-                if (!visitado[i][j] && tablero[i][j] != ' ') {
-                    char color = tablero[i][j];
+        boolean[][] visitado = new boolean[tablero.getFilas()][tablero.getColumnas()];
+        
+        for (int i = 0; i < tablero.getFilas(); i++) {
+            for (int j = 0; j < tablero.getColumnas(); j++) {
+                Ficha ficha = tablero.getFicha(i, j);
+                if (!visitado[i][j] && ficha.getColor() != ' ') {
+                    char color = ficha.getColor();
                     ArrayList<int[]> grupo = new ArrayList<>();
                     getMovsRecurs(i, j, color, visitado, grupo);
-                    if (grupo.size()>1){
+                    if (grupo.size() > 1) {
                         movsPosib.add(grupo);
                     }
                 }
             }
         }
-        private void getMovsRecurs(int fila, int columna, char color, boolean[][] visitado, ArrayList<int[]> grupo) {
-            if (fila < 0 || fila >= fil || columna < 0 || columna >= col || visitado[fila][columna]) {
-                return;
-            }
-            char ficha = tablero[fila][columna];
-            if (ficha == ' ' || ficha != color) {
-                return;
-            }
-            visitado[fila][columna] = true;
-            grupo.add(new int[] { fila, columna });
-            getMovsRecurs(fila - 1, columna, color, visitado, grupo); // miro las fichas de encima
-            getMovsRecurs(fila + 1, columna, color, visitado, grupo); // "" las de abajo
-            getMovsRecurs(fila, columna - 1, color, visitado, grupo); // "" las de izquierda
-            getMovsRecurs(fila, columna + 1, color, visitado, grupo); // ""las de derecha
+        
+        return movsPosib;
+    }
+
+    private void getMovsRecurs(int fila, int columna, char color, boolean[][] visitado, ArrayList<int[]> grupo) {
+        if (fila < 0 || fila >= tablero.getFilas() || columna < 0 || columna >= tablero.getColumnas() || visitado[fila][columna]) {
+            return;
         }
 
-*/
+        Ficha ficha = tablero.getFicha(fila, columna);
+        if (ficha.getColor() == ' ' || ficha.getColor() != color) {
+            return;
+        }
+
+        visitado[fila][columna] = true;
+        grupo.add(new int[] { fila, columna });
+
+        getMovsRecurs(fila - 1, columna, color, visitado, grupo); // Miro las fichas de encima
+        getMovsRecurs(fila + 1, columna, color, visitado, grupo); // "" las de abajo
+        getMovsRecurs(fila, columna - 1, color, visitado, grupo); // "" las de izquierda
+        getMovsRecurs(fila, columna + 1, color, visitado, grupo); // "" las de derecha
+    }
+
     public void imprimirTablero() {
         for (ArrayList<Ficha> fila : tablero) {
             for (Ficha ficha : fila) {
